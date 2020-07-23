59615a2cad0d54b0fa2755a7dece2d41dfcf4918
amitj-coder <amitj-coder@users.noreply.github.com>
2020-07-23 22:16:31 +0530

    Bugs Fixed

--- VissionBoard/settings.py
+++ VissionBoard/settings.py
@@ -142,7 +142,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "board/static"),
-    os.path.join(BASE_DIR, "home/static"),
+    os.path.join(BASE_DIR, "home/static")
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+
 from. import views
+
 urlpatterns = [
     path('', views.home, name='home-home'),
     path('about/', views.about, name='home-about'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
